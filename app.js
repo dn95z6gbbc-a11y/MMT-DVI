05f5d4ea0df9443d332c3d15208e10596ada650c
dn95z6gbbc-a11y <dn95z6gbbc@privaterelay.appleid.com>
2026-08-16 10:02:37 +0300

    Load first-run clarity patch

--- app.js
+++ app.js
@@ -23,7 +23,8 @@
     ['v0123.js','0123'],
     ['v0124.js','0124b'],
     ['v0125.js','0125'],
-    ['v0126.js','0126']
+    ['v0126.js','0126'],
+    ['v0127.js','0127']
   ];
 
   window.MMT_MODULES=modules.map(([file,version])=>({file,version}));
