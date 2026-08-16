7a2e36a94ebd5eee147909fda1c1807a18869e23
dn95z6gbbc-a11y <dn95z6gbbc@privaterelay.appleid.com>
2026-08-16 11:01:58 +0300

    Load v0.13.3 route creation fix

--- app.js
+++ app.js
@@ -30,7 +30,8 @@
     ['v01210.js','01210'],
     ['v013.js','013'],
     ['v0131.js','0131'],
-    ['v0132.js','0132']
+    ['v0132.js','0132'],
+    ['v0133.js','0133']
   ];
 
   window.MMT_MODULES=modules.map(([file,version])=>({file,version}));
