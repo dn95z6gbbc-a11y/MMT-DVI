f9656abe985e2750bcc9438ac920fbdfb078e269
dn95z6gbbc-a11y <dn95z6gbbc@privaterelay.appleid.com>
2026-08-16 11:23:50 +0300

    Load v0.13.4 verdict navigation fix

--- app.js
+++ app.js
@@ -31,7 +31,8 @@
     ['v013.js','013'],
     ['v0131.js','0131'],
     ['v0132.js','0132'],
-    ['v0133.js','0133c']
+    ['v0133.js','0133c'],
+    ['v0134.js','0134']
   ];
 
   window.MMT_MODULES=modules.map(([file,version])=>({file,version}));
