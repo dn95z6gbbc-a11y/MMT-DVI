7281b6b34d4669f04a2072d1ed00c09d63fe17fb
dn95z6gbbc-a11y <dn95z6gbbc@privaterelay.appleid.com>
2026-08-16 09:13:54 +0300

    Load v0.12.1 sanitation module

--- app.js
+++ app.js
@@ -17,7 +17,8 @@
     ['v010-fix.js','0101'],
     ['v010-extra.js','0102'],
     ['v011.js','011'],
-    ['v012.js','012']
+    ['v012.js','012'],
+    ['v0121.js','0121']
   ];
 
   window.MMT_MODULES=modules.map(([file,version])=>({file,version}));
