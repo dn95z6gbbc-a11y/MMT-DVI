47f0c15c78107702f05965607633aa0493f14b4e
dn95z6gbbc-a11y <dn95z6gbbc@privaterelay.appleid.com>
2026-08-16 18:10:41 +0300

    load v0.13.8 news rubric

--- app.js
+++ app.js
@@ -35,7 +35,8 @@
     ['v0134.js','0134'],
     ['v0135.js','0135'],
     ['v0136.js','0136'],
-    ['v0137.js','0137']
+    ['v0137.js','0137'],
+    ['v0138.js','0138']
   ];
 
   window.MMT_MODULES=modules.map(([file,version])=>({file,version}));
