f0d00d89d23631bb947056b961ae63316236bc6b
dn95z6gbbc-a11y <dn95z6gbbc@privaterelay.appleid.com>
2026-08-18 12:40:00 +0300

    Connect MMT DVI AI review to Cloudflare Worker

--- mmt-config.js
+++ mmt-config.js
@@ -1,4 +1,4 @@
 /* MMT ДВИ runtime configuration. No secrets belong in this file. */
 window.MMT_CONFIG = Object.assign({
-  aiReviewEndpoint: ''
+  aiReviewEndpoint: 'https://mmt-dvi-review.mushkovmedia.workers.dev/'
 }, window.MMT_CONFIG || {});
