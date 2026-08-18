86697a5ccfeba21f707182c35b67bfdcc31875d3
dn95z6gbbc-a11y <dn95z6gbbc@privaterelay.appleid.com>
2026-08-18 14:16:01 +0300

    Load v0.14.7 human-readable AI review labels

--- app.js
+++ app.js
@@ -44,7 +44,8 @@
     ['v0142.js','0142'],
     ['v0143.js','0143'],
     ['v0144.js','0146'],
-    ['v0145.js','0146']
+    ['v0145.js','0146'],
+    ['v0147.js','0147']
   ];
 
   window.MMT_MODULES=modules.map(([file,version])=>({file,version}));
