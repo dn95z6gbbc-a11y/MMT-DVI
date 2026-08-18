eb3110f166af4f28d3e3cfbce673db8f3466efab
dn95z6gbbc-a11y <dn95z6gbbc@privaterelay.appleid.com>
2026-08-18 14:06:50 +0300

    Bust cache for calibrated AI client v0.14.6

--- app.js
+++ app.js
@@ -43,8 +43,8 @@
     ['v0141.js','0141'],
     ['v0142.js','0142'],
     ['v0143.js','0143'],
-    ['v0144.js','0144'],
-    ['v0145.js','0145']
+    ['v0144.js','0146'],
+    ['v0145.js','0146']
   ];
 
   window.MMT_MODULES=modules.map(([file,version])=>({file,version}));
