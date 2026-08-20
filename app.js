3ed33de16b105a041c85b732030f8e89ffb82b80
dn95z6gbbc-a11y <dn95z6gbbc@privaterelay.appleid.com>
2026-08-20 18:17:14 +0300

    Use stable interview methodology module v0.16.2

--- app.js
+++ app.js
@@ -50,7 +50,7 @@
     ['v0151.js','0151'],
     ['v0152.js','0152'],
     ['v016.js','0160'],
-    ['v0161.js','0161']
+    ['v0162.js','0162']
   ];
 
   window.MMT_MODULES=modules.map(([file,version])=>({file,version}));
